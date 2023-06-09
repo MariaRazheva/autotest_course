e2b326e60a5379958e32af11f3fdc4c59e00fa42
MariaRazheva <133018027+MariaRazheva@users.noreply.github.com>
2023-06-09 07:08:39 +0300

    Add files via upload

---
+++ task_10_3.py
@@ -0,0 +1,26 @@
+# Из набора тестов задания task_2 создайте один тест с параметрами, используя @pytest.mark.parametrize
+# Промаркируйте 1 параметр из выборки как smokе, а 1 набор данных скипните
+
+
+import pytest
+
+
+@pytest.mark.parametrize('in_data, out', [((1, 1), 1), ((1, 0), pytest.raises(ZeroDivisionError)), ((0, 1), 0), ((1, -1, -1), 1), ((256, 2, 2, 2), 32)])
+def test_all_division(in_data, out):
+    if not pytest.raises(ZeroDivisionError):
+        assert all_division(*in_data) == out
+
+
+
+#def test2():
+    # #assert all_division(1, 0) == ZeroDivisionError
+    # with pytest.raises(ZeroDivisionError):
+    #     all_division(1, 0)
+
+
+def all_division(*arg1):
+    division = arg1[0]
+    for i in arg1[1:]:
+        division /= i
+    print(division)
+    return division
